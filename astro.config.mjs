d2eba049db582f2d9e40b495524e0ffb06fd3e93
XuFan <150409317+Littlefun8@users.noreply.github.com>
2026-04-10 21:00:48 +0800

    Update site and base properties for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. GitHub Pages 完整地址
+  site: 'https://Littlefun8.github.io',
+  // 2. 仓库名作为子路径
+  base: '/pm-portfolio', 
   vite: {
     plugins: [tailwindcss()],
   },
